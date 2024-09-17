ee1a0c382a1ffaa18fdde185cae9e78e4ee73f95
EUNOIA <budianggara050788>
2024-09-17 16:12:00 +0700

    update

---
+++ kuliah/pertemuan10/latihan2.php
@@ -0,0 +1,58 @@
+<?php 
+//Koneksi ke DB dan pilih database
+$conn = mysqli_connect('localhost','root','','pw_budianggara');
+
+//Query isi tabel mahasiswa
+$result = mysqli_query($conn,"SELECT * FROM mahasiswa");
+
+
+
+//Ubah data kedalam array
+$rows = [];
+while($row = mysqli_fetch_assoc($result)){
+  $rows[]=$row;
+}
+
+//Tampung ke variabel mahasiswa
+$mahasiswa = $rows;
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+<body>
+  <h3>Daftar Mahasiswa</h3>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <?php $i=1;
+    foreach($mahasiswa as $m): ?>
+    <tr>
+      <td><?= $i++; ?></td>
+      <td><img src="images/<?= $m['gambar'];?>" width="60"></td>
+      <td><?= $m['nrp']; ?></td>
+      <td><?= $m['nama']; ?></td>
+      <td><?= $m['email']; ?></td>
+      <td><?= $m['jurusan']; ?></td>
+      <td>
+        <a href="#">Update</a> | <a href="#">Delete</a>
+      </td>
+    </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+</html>
